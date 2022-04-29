2a0768762937f3f411f864828332d14e810f26bd
Lucas <lucasbastos@ilove.me>
2022-04-29 16:30:11 -0300

    feat: list students table and sidebar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,56 +1,169 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <div class="container">
+    <div class="sidenav">
+      <img src="./assets/logo-grupo-a-1.svg" alt="">
+      <span>Módulo Acadêmico</span>
+      <a href="#">Alunos</a>
+    </div>
+    <div class="content">
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-text-field label="Digite sua busca" solo></v-text-field>
+          </v-col>
+          <v-col>
+            <v-btn>
+              Cadastrar Aluno
+            </v-btn>
+          </v-col>
+        </v-row>
+          <v-data-table :headers="headers" :items="students" sort-by="ra" class="elevation-1">
+            <template v-slot:[`item.actions`]="{ item }">
+              <v-btn color="blue-grey" class="ma-2 white--text" @click="editItem(item)">
+                Editar
+                <v-icon right dark>
+                  mdi-pencil
+                </v-icon>
+              </v-btn>
+              <v-btn @click="editItem(item)" color="blue-grey" class="ma-2 white--text">
+                Excluir
+                <v-icon right dark>
+                  mdi-delete
+                </v-icon>
+              </v-btn>
+            </template>
+          </v-data-table>
+      </v-container>
+    </div>
+    <Dialog />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import axios from 'axios';
+import Dialog from './components/Dialog.vue';
 
 export default Vue.extend({
   name: 'App',
+  components: {
+    Dialog,
+  },
 
   data: () => ({
     //
+    students: [],
+    dialog: false,
+    headers: [
+      {
+        text: 'Registro Acadêmico',
+        align: 'center',
+        sortable: true,
+        value: 'ra',
+      },
+      {
+        text: 'Nome',
+        align: 'center',
+        sortable: true,
+        value: 'name',
+      },
+      {
+        text: 'CPF',
+        align: 'start',
+        sortable: true,
+        value: 'cpf',
+      },
+      {
+        text: 'Ações',
+        align: 'start',
+        sortable: true,
+        value: 'actions',
+      },
+    ],
+    drawer: false,
+    group: null,
   }),
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+
+  mounted() {
+    axios.get('http://localhost:3000/api/v1/students')
+      .then((response) => {
+        this.students = response.data;
+      });
+  },
+
+  methods: {
+    editItem(item: any) {
+      //
+      console.log(item);
+    },
+  },
 });
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins');
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+
+img {
+  width: 80%;
+}
+
+.content__search {
+  width: 50%;
+}
+
+.sidenav {
+  height: 100%;
+  width: 200px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.sidenav span {
+  font-size: 15px;
+  font-weight: bold;
+  margin-top: 20px;
+  background-color: #f1f1f1;
+  border-radius: 6px;
+  padding: 10px;
+}
+
+.sidenav a {
+  text-decoration: none;
+  font-size: 18px;
+  color: #818181;
+  display: block;
+  margin-top: 10px;
+}
+
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+.content {
+  margin-left: 200px;
+}
+
+.content__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,16 +1,16 @@
 <template>
-  <hello-world />
+  <Dialog />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '../components/HelloWorld.vue';
+import Dialog from '../components/Dialog.vue';
 
 export default Vue.extend({
   name: 'Home',
 
   components: {
-    HelloWorld,
+    Dialog,
   },
 });
 </script>
